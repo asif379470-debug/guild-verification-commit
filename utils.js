e70a4b163146640116060e2b628300cfa341340e
asif379470-debug <asif379470@gmail.com>
2026-03-14 22:03:24 +0600

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T16:02:53.478Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-14T16:03:23.040Z
+Commit: Fix broken links in documentation
